63974d54eb19ae85613beefb14e5980158727395
Bob <petrbobe@seznam.cz>
2024-05-03 13:14:03 +0200

    ICMP Ping for one address

--- ICMPPing/Program.cs
+++ ICMPPing/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ICMPPing
@@ -10,6 +12,35 @@ namespace ICMPPing
     {
         static void Main(string[] args)
         {
+            string address = "cs.wikipedia.org";
+            int seconds = 60;
+
+            Console.WriteLine($"Ping na adresu: {address} po dobu {seconds} sekund v intervalu 100ms");
+
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+            options.DontFragment = true;
+
+            byte[] buffer = new byte[32];
+            int timeout = 300;
+
+            DateTime endTime = DateTime.Now.AddSeconds(seconds);
+
+            int successfulPings = 0;
+            int totalPings = 0;
+            while (DateTime.Now < endTime)
+            {
+                PingReply reply = pingSender.Send(address, timeout, buffer, options);
+                //Console.WriteLine($"Ping odpověď z {reply.Address}: Odpověď={reply.Status}, Čas={reply.RoundtripTime}ms");
+                if (reply.Status == IPStatus.Success)
+                    successfulPings++;
+
+                totalPings++;
+
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine($"Dostupnost pro {address} je: {successfulPings * 100 / totalPings}%");
         }
     }
 }
