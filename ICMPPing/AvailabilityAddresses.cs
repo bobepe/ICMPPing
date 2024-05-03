e4b8a1a7193facf4fdc65e9ca1f53200b163c186
Bob <petrbobe@seznam.cz>
2024-05-03 15:57:10 +0200

    class availability of adress

---
+++ ICMPPing/AvailabilityAddresses.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICMPPing
+{
+    public class AvailabilityAddresses
+    {
+        public int Total { get; set; }
+        public int Succes { get; set; }
+        public int AvailabilityPercentages
+        {
+            get
+            {
+                return Succes * 100 / Total;
+            }
+        }
+    }
+}
